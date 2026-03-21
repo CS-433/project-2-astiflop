93bcac83afebc08daaee6da042ba9e3683b84862
elrbe <elrbergeron@gmail.com>
2026-03-21 12:56:00 +0100

    fix: allow importation of utils

--- scripts/plot_results.py
+++ scripts/plot_results.py
@@ -1,5 +1,8 @@
 import json
 import argparse
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from utils.plot_utils.presents_results import plot_results
 
 if __name__ == "__main__":
